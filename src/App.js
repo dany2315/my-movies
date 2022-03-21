c7b9263a855738ebe9974b8c9d581844441ebd54
Dany serfaty <davidserfaty2315@gmail.com>
2022-03-21 22:30:19 +0200

    initialisation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react'
+import { Movies } from './components/Movies';
+
+
+function App() {
+    return(
+        <div>
+            <Movies/>
+        </div>
+    )
+}
+
+
+export default App
---
+++ src/components/Movies.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {getMovies} from '../services/fakeMovieService';
+
+export class Movies extends Component {
+    constructor() {
+        super();
+        this.state={
+            movies:getMovies()
+        }
+    }
+
+   handelClick =(movie)=>{
+       const movies=this.state.movies.filter((m)=>m._id!== movie.id)
+       this.setState.movies({movies:movies}) 
+   }
+    
+    render() { 
+        return<div>
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Titre</th>
+                    <th>Genre</th>
+                    <th>Stock</th>
+                    <th>Note</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {this.state.movies.map ((movie)=>{
+                    <tr key={movie._id}>
+                        <td>{movie.title}</td>
+                        <td>{movie.genre}</td>
+                        <td>{movie.numberInStock}</td>
+                        <td>{movie.dailyRentalRate}</td>
+                        <td>
+                            <button onClick={()=>{this.handelClick(movie)}} className="btn btn-danger">
+                                supprimer
+                            </button>
+                        </td>
+                    </tr>
+                })}
+            </tbody>
+        </table>
+        </div>
+       
+        ;
+    }
+}
+ 
+
